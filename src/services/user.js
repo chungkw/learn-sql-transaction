3954a2d44d3836ca0d77fff315e45d5b82dd3bef
Chung Kin Wai <chungkw28@gmail.com>
2022-04-23 13:44:58 +0800

    Update user.js

--- src/services/user.js
+++ src/services/user.js
@@ -4,19 +4,38 @@ module.exports = {
     async createOne(username, description) {
         const conn = await pool.getConnection();
 
-        const sql = `
-            INSERT INTO user (username, description)
-            VALUES (?, ?);
-        `;
+        // start a transaction on the connection
+        await conn.beginTransaction();
 
-        const [rows, fields] = await conn.query(sql, [username, description]);
+        try {
+            const sql = `
+                INSERT INTO user (username, description)
+                VALUES (?, ?);
+            `;
 
-        conn.release();
-        return rows[0];
+            const [rows, fields] = await conn.query(sql, [username, description]);
+
+            // commit the changes
+            await conn.commit();
+
+            conn.release();
+            return rows[0];
+        }
+        catch (error) {
+            // undo changes
+            await conn.rollback();
+
+            // should release a connection always
+            conn.release();
+            // propagate error to caller
+            throw error;
+        }
     },
 
     async createOneFaulty(username, description) {
         const conn = await pool.getConnection();
+
+        // start a transaction on the connection
         await conn.beginTransaction();
 
         try {
@@ -29,12 +48,19 @@ module.exports = {
 
             throw new Error('a stupid error for no good reason');
 
+            // commit the changes
             await conn.commit();
+
             conn.release();
             return rows[0];
         }
         catch (error) {
+            // undo changes
             await conn.rollback();
+
+            // should release a connection always
+            conn.release();
+            // propagate error to caller
             throw error;
         }
     },
@@ -42,27 +68,39 @@ module.exports = {
     async getMany() {
         const conn = await pool.getConnection();
 
-        const sql = `
-            SELECT * FROM user
-        `;
+        try {
+            const sql = `
+                SELECT * FROM user
+            `;
 
-        const [rows, fields] = await conn.query(sql);
+            const [rows, fields] = await conn.query(sql);
 
-        conn.release();
-        return rows;
+            conn.release();
+            return rows;
+        }
+        catch (error) {
+            conn.release();
+            throw error;
+        }
     },
 
     async getOne(userId) {
         const conn = await pool.getConnection();
 
-        const sql = `
-            SELECT * FROM user
-            WHERE user_id = ?
-        `;
+        try {
+            const sql = `
+                SELECT * FROM user
+                WHERE user_id = ?
+            `;
 
-        const [rows, fields] = await conn.query(sql, [userId]);
+            const [rows, fields] = await conn.query(sql, [userId]);
 
-        conn.release();
-        return rows[0];
+            conn.release();
+            return rows[0];
+        }
+        catch (error) {
+            conn.release();
+            throw error;
+        }
     }
 };
